28d50f99c07843340e3c96150bb74e45cdbfb2b7
JeFlame <hoangvu130301@gmail.com>
2022-04-18 16:17:30 +0700

    add color-picker component

---
+++ color-picker/src/App.js
@@ -0,0 +1,16 @@
+import './ColorPicker';
+import './App.css';
+import ColorPicker from './ColorPicker';
+
+function App() {
+  return (
+    <>
+    <main id="app">
+      <ColorPicker />
+    </main>
+    <script src="https://content.codecademy.com/courses/React/react-16.bundle.js"></script>
+    </>
+  );
+}
+
+export default App;
